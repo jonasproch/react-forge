0b8c2051c62a15fe0e317103bb46c50f3932170e
Jonas <jonasprochaz@gmail.com>
2025-06-02 17:39:33 +0200

    use commander

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
+import { Command } from 'commander'
 import createNextApp from './builder/createNextApp.js'
 import createViteApp from './builder/createViteApp.js'
 import { Framework } from './prompts/options.js'
 import promptUser from './prompts/promptUser.js'
 import installPM from './utils/installPM.js'
+import packageJson from '../package.json' with { type: 'json' }
+
+const program = new Command(packageJson.name)
+
+program
+    .description('Create a React project with auto setup')
+    .version(packageJson.version, '-v, --version', `Output the current version of ${packageJson.name}`)
 
 const { projectName, framework, settings } = await promptUser()
 
@@ -19,6 +27,9 @@ try {
         case Framework.Vite:
             await createViteApp(projectName, settings)
     }
+
+    process.exit(0)
 } catch (err) {
     console.log(`An error occurred while running React Forge: \n${err}`)
+    process.exit(1)
 }
